28cfc0ca7f694ced11283cbe3eaa83edb76d4ea1
csantabearing <camilo@bearing.ai>
2024-04-02 12:45:40 -0500

    openai

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from rag_qa import qa
 app = FastAPI()
 
 
-@app.get("/query", summary='Query with llamaindex')
+@app.get("/query", summary='Query with llamaindex+openai')
 def root(question:str):
     return qa(question)
