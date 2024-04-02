ec8c696e48c3a1b9e372440d9785d8d3640eeac6
csantabearing <camilo@bearing.ai>
2024-04-02 12:34:00 -0500

    tests

--- test/test_ragas.py
+++ test/test_ragas.py
@@ -1,35 +1,34 @@
-# import pandas as pd
-# from datasets import Dataset
-# from ragas.metrics import (
-#     answer_relevancy,
-#     faithfulness,
-#     context_recall,
-#     context_precision,
-# )
-#from ragas import evaluate
-#from rag_qa import qa
+import pandas as pd
+from datasets import Dataset
+from ragas.metrics import (
+    answer_relevancy,
+    faithfulness,
+    context_recall,
+    context_precision,
+)
+from ragas import evaluate
+from rag_qa import qa
 
 def test_qa():
-    assert True
-    # df=pd.read_csv('test/paul_synthetic.csv')
-    # answers=[]
-    # contexts=[]
-    # for question in df['question']:
-    #     response=qa(question)
-    #     answers.append(response.response)
-    #     contexts.append([context.text for context in response.source_nodes])
-    # df['answer']=answers
-    # df['contexts']=contexts
-    # result = evaluate(
-    #     Dataset.from_pandas(df),
-    #     metrics=[
-    #         context_precision,
-    #         faithfulness,
-    #         answer_relevancy,
-    #         context_recall,
-    #     ],
-    # )
-    # assert result['context_precision']>0.6
-    # assert result['faithfulness']>0.8
-    # assert result['answer_relevancy']>0.8
-    # assert result['context_recall']>0.7
+    df=pd.read_csv('test/paul_synthetic.csv').iloc[:2]
+    answers=[]
+    contexts=[]
+    for question in df['question']:
+        response=qa(question)
+        answers.append(response.response)
+        contexts.append([context.text for context in response.source_nodes])
+    df['answer']=answers
+    df['contexts']=contexts
+    result = evaluate(
+        Dataset.from_pandas(df),
+        metrics=[
+            context_precision,
+            faithfulness,
+            answer_relevancy,
+            context_recall,
+        ],
+    )
+    assert result['context_precision']>0.6
+    assert result['faithfulness']>0.8
+    assert result['answer_relevancy']>0.8
+    assert result['context_recall']>0.7
